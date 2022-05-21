1eed1eeb9d91fe51ed43b2b7d09ac857de514abf
psandovals <pedrosandoval0803@outlook.com>
2022-05-21 01:59:32 -0600

    lectura de datos de archivo json

--- principal.py
+++ principal.py
@@ -3,4 +3,17 @@ import nltk as nt
 import tensorflow as tf
 import tflearn as tl
 import numpy as np 
+import json 
+import random 
+import pickle
  
+ #CONVERTIR MÁS LEGIBLE NUESTRO CHAT
+    #stemmer = nt.stem.lancaster.LancasterStemmer()
+
+
+#OBTENEMOS LOS DATOS DE NUESTRO ARCHIVO JSON
+with open("respuestas.json") as f:
+    contenido = json.load(f)
+
+#IMPRIMIMOS EN PANTALLA LA CARGA DEL ARCHIVO JSON
+print(contenido)
